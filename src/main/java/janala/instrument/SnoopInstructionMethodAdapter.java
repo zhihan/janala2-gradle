aebca720ad966a8bb8be672b06c81bafca95b29d
Zhi Han <zhi.han@gmail.com>
2015-08-21 10:37:25 -0400

    Refactor instrumentation code for testing. Added tests for method instrumentation.

--- src/main/java/janala/instrument/SnoopInstructionMethodAdapter.java
+++ src/main/java/janala/instrument/SnoopInstructionMethodAdapter.java
@@ -32,105 +32,12 @@ public class SnoopInstructionMethodAdapter extends MethodVisitor implements Opco
     mv.visitCode();
   }
 
-  private void addBipushInsn(MethodVisitor mv, int val) {
-    switch (val) {
-      case 0:
-        mv.visitInsn(ICONST_0);
-        break;
-      case 1:
-        mv.visitInsn(ICONST_1);
-        break;
-      case 2:
-        mv.visitInsn(ICONST_2);
-        break;
-      case 3:
-        mv.visitInsn(ICONST_3);
-        break;
-      case 4:
-        mv.visitInsn(ICONST_4);
-        break;
-      case 5:
-        mv.visitInsn(ICONST_5);
-        break;
-      default:
-        mv.visitLdcInsn(new Integer(val));
-        break;
-    }
+  private static void addBipushInsn(MethodVisitor mv, int val) {
+    Utils.addBipushInsn(mv, val);
   }
 
   private void addValueReadInsn(MethodVisitor mv, String desc, String methodNamePrefix) {
-    Type t;
-
-    if (desc.startsWith("(")) {
-      t = Type.getReturnType(desc);
-    } else {
-      t = Type.getType(desc);
-    }
-    switch (t.getSort()) {
-      case Type.DOUBLE:
-        mv.visitInsn(DUP2);
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "double", "(D)V");
-        break;
-      case Type.LONG:
-        mv.visitInsn(DUP2);
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "long", "(J)V");
-        break;
-      case Type.ARRAY:
-        mv.visitInsn(DUP);
-        mv.visitMethodInsn(
-            INVOKESTATIC,
-            Config.instance.analysisClass,
-            methodNamePrefix + "Object",
-            "(Ljava/lang/Object;)V");
-        break;
-      case Type.BOOLEAN:
-        mv.visitInsn(DUP);
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "boolean", "(Z)V");
-        break;
-      case Type.BYTE:
-        mv.visitInsn(DUP);
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "byte", "(B)V");
-        break;
-      case Type.CHAR:
-        mv.visitInsn(DUP);
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "char", "(C)V");
-        break;
-      case Type.FLOAT:
-        mv.visitInsn(DUP);
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "float", "(F)V");
-        break;
-      case Type.INT:
-        mv.visitInsn(DUP);
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "int", "(I)V");
-        break;
-      case Type.OBJECT:
-        mv.visitInsn(DUP);
-        mv.visitMethodInsn(
-            INVOKESTATIC,
-            Config.instance.analysisClass,
-            methodNamePrefix + "Object",
-            "(Ljava/lang/Object;)V");
-        break;
-      case Type.SHORT:
-        mv.visitInsn(DUP);
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "short", "(S)V");
-        break;
-      case Type.VOID:
-        mv.visitMethodInsn(
-            INVOKESTATIC, Config.instance.analysisClass, methodNamePrefix + "void", "()V");
-        break;
-      default:
-        System.err.println("Unknown field or method descriptor " + desc);
-        System.exit(1);
-    }
+    Utils.addValueReadInsn(mv, desc, methodNamePrefix);
   }
 
   private void addSpecialInsn(MethodVisitor mv, int val) {
--- src/test/groovy/MethodInstrumenterTest.groovy
+++ src/test/groovy/MethodInstrumenterTest.groovy
@@ -33,19 +33,255 @@ class MethodInstrumenterTest {
     ma = new SnoopInstructionMethodAdapter(recorder.getVisitor(), false)
   }
 
-  @Test
-  void testICONST_0() {
-    ma.visitInsn(Opcodes.ICONST_0)
+  private void testInsn(int opcode, String name) {
+    ma.visitInsn(opcode)
 
     MethodRecorder expected = new MethodRecorder()
     def ev = expected.getVisitor()
     ev.visitInsn(Opcodes.ICONST_1)
     ev.visitInsn(Opcodes.ICONST_0)
-    ev.visitMethodInsn(Opcodes.INVOKESTATIC, 
-      Config.instance.analysisClass, "ICONST_0", "(II)V")
-    ev.visitInsn(Opcodes.ICONST_0)
-    
+    ev.visitMethodInsn(Opcodes.INVOKESTATIC,
+      Config.instance.analysisClass, name, "(II)V")
+    ev.visitInsn(opcode)
+
     assertEquals(expected, recorder)
+  }
+
+  @Test
+  void testNOP() {
+    testInsn(Opcodes.NOP, "NOP")
+  }
+
+  @Test
+  void testACONST_NULL() {
+    testInsn(Opcodes.ACONST_NULL, "ACONST_NULL")
+  }
+
+  @Test
+  void testICONST_M1() {
+    testInsn(Opcodes.ICONST_M1, "ICONST_M1")
+  }
+
+  @Test
+  void testICONST_0() {
+    testInsn(Opcodes.ICONST_0, "ICONST_0")
+  }
+
+  @Test
+  void testICONST_1() {
+    testInsn(Opcodes.ICONST_1, "ICONST_1")
+  }
+
+  @Test
+  void testICONST_2() {
+    testInsn(Opcodes.ICONST_2, "ICONST_2")
+  }
+
+  @Test
+  void testICONST_3() {
+    testInsn(Opcodes.ICONST_3, "ICONST_3")
+  }
+
+  @Test
+  void testICONST_4() {
+    testInsn(Opcodes.ICONST_4, "ICONST_4")
+  }
+
+  @Test
+  void testICONST_5() {
+    testInsn(Opcodes.ICONST_5, "ICONST_5")
+  }
+
+  @Test
+  void testLCONST_0() {
+    testInsn(Opcodes.LCONST_0, "LCONST_0")
+  }
+
+  @Test
+  void testLCONST_1() {
+    testInsn(Opcodes.LCONST_1, "LCONST_1")
+  }
+
+  @Test
+  void testFCONST_0() {
+    testInsn(Opcodes.FCONST_0, "FCONST_0")
+  }
+
+  @Test
+  void testFCONST_1() {
+    testInsn(Opcodes.FCONST_1, "FCONST_1")
+  }
+
+  @Test
+  void testFCONST_2() {
+    testInsn(Opcodes.FCONST_2, "FCONST_2")
+  }
+
+  @Test
+  void testDCONST_0() {
+    testInsn(Opcodes.DCONST_0, "DCONST_0")
+  }
+
+  @Test
+  void testDCONST_1() {
+    testInsn(Opcodes.DCONST_1, "DCONST_1")
+  }
+
+  @Test
+  void testPOP() {
+    testInsn(Opcodes.POP, "POP")
+  }
+
+  @Test
+  void testPOP2() {
+    testInsn(Opcodes.POP2, "POP2")
+  }
+
+  @Test
+  void testDUP() {
+    testInsn(Opcodes.DUP, "DUP")
+  }
+
+  @Test
+  void testDUP_X1() {
+    testInsn(Opcodes.DUP_X1, "DUP_X1")
+  }
+
+  @Test
+  void testDUP_X2() {
+    testInsn(Opcodes.DUP_X2, "DUP_X2")
+  }
+
+  @Test
+  void testDUP2() {
+    testInsn(Opcodes.DUP2, "DUP2")
+  }
+
+  @Test
+  void testDUP2_X1() {
+    testInsn(Opcodes.DUP2_X1, "DUP2_X1")
+  }
+
+  @Test
+  void testDUP2_X2() {
+    testInsn(Opcodes.DUP2_X2, "DUP2_X2")
+  }  
+  private void testInsnWithExceptionAndValue(int opcode, 
+      String name, String type, String prefix) {
+    ma.visitInsn(opcode)
+    
+    MethodRecorder expected = new MethodRecorder()
+    def ev = expected.getVisitor()
+    ev.visitInsn(Opcodes.ICONST_1)
+    ev.visitInsn(Opcodes.ICONST_0)
+    ev.visitMethodInsn(Opcodes.INVOKESTATIC,
+      Config.instance.analysisClass, name, "(II)V")
+    ev.visitInsn(opcode)
+
+    Utils.addBipushInsn(ev, 0) // exception-free path
+    ev.visitMethodInsn(Opcodes.INVOKESTATIC, 
+      Config.instance.analysisClass, "SPECIAL", "(I)V");
+    Utils.addValueReadInsn(ev, type, prefix)
+
+    assertEquals(expected, recorder)
+  }
+
+  @Test
+  void testIALOAD() {
+    testInsnWithExceptionAndValue(Opcodes.IALOAD, "IALOAD", "I", "GETVALUE_")
+  }
+
+  @Test
+  void testLALOAD() {
+    testInsnWithExceptionAndValue(Opcodes.LALOAD, "LALOAD", "J", "GETVALUE_")
+  }
+
+  @Test
+  void testFALOAD() {
+    testInsnWithExceptionAndValue(Opcodes.FALOAD, "FALOAD", "F", "GETVALUE_")
+  }
+
+  @Test
+  void testDALOAD() {
+    testInsnWithExceptionAndValue(Opcodes.DALOAD, "DALOAD", "D", "GETVALUE_")
+  }
+
+  @Test
+  void testBALOAD() {
+    testInsnWithExceptionAndValue(Opcodes.BALOAD, "BALOAD", "B", "GETVALUE_")
+  }
+
+  @Test
+  void testCALOAD() {
+    testInsnWithExceptionAndValue(Opcodes.CALOAD, "CALOAD", "C", "GETVALUE_")
+  }
+
+  @Test
+  void testSALOAD() {
+    testInsnWithExceptionAndValue(Opcodes.SALOAD, "SALOAD", "S", "GETVALUE_")
+  }
+
+  @Test
+  void testAALOAD() {
+    testInsnWithExceptionAndValue(Opcodes.AALOAD, "AALOAD", "Ljava/lang/Object;", "GETVALUE_")
+  }
+
+  private void testInsnWithException(int opcode, String name, String type) {
+    ma.visitInsn(opcode)
+    
+    MethodRecorder expected = new MethodRecorder()
+    def ev = expected.getVisitor()
+    ev.visitInsn(Opcodes.ICONST_1)
+    ev.visitInsn(Opcodes.ICONST_0)
+    ev.visitMethodInsn(Opcodes.INVOKESTATIC,
+      Config.instance.analysisClass, name, "(II)V")
+    ev.visitInsn(opcode)
+
+    Utils.addBipushInsn(ev, 0) // exception-free path
+    ev.visitMethodInsn(Opcodes.INVOKESTATIC, 
+      Config.instance.analysisClass, "SPECIAL", "(I)V");
+
+    assertEquals(expected, recorder)
+  }
+
+  @Test
+  void testIASTORE() {
+    testInsnWithException(Opcodes.IASTORE, "IASTORE", "I")
+  }
+
+  @Test
+  void testLASTORE() {
+    testInsnWithException(Opcodes.LASTORE, "LASTORE", "J")
+  }
+
+  @Test
+  void testFASTORE() {
+    testInsnWithException(Opcodes.FASTORE, "FASTORE", "F")
+  }
+
+  @Test
+  void testDASTORE() {
+    testInsnWithException(Opcodes.DASTORE, "DASTORE", "D")
+  }
+
+  @Test
+  void testBASTORE() {
+    testInsnWithException(Opcodes.BASTORE, "BASTORE", "B")
+  }
+
+  @Test
+  void testCASTORE() {
+    testInsnWithException(Opcodes.CASTORE, "CASTORE", "C")
+  }
+
+  @Test
+  void testSASTORE() {
+    testInsnWithException(Opcodes.SASTORE, "SASTORE", "S")
+  }
+
+  @Test
+  void testAASTORE() {
+    testInsnWithException(Opcodes.AASTORE, "AASTORE", "Ljava/lang/Object;")
   }
 
   @Test
@@ -64,7 +300,7 @@ class MethodInstrumenterTest {
     ev.visitMethodInsn(Opcodes.INVOKESTATIC,
       Config.instance.analysisClass, "GETVALUE_int", "(I)V")
 
-    assertEquals(expected, recorder)  
+    assertEquals(expected, recorder)
   }
 
   @Test
@@ -80,6 +316,6 @@ class MethodInstrumenterTest {
       Config.instance.analysisClass, "ISTORE", "(III)V")
     ev.visitVarInsn(Opcodes.ISTORE, 1)
 
-    assertEquals(expected, recorder)   
+    assertEquals(expected, recorder)
   }
 }
