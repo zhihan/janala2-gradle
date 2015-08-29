d7e1ea175ae4a774dfad07c0cd19258f53c92e8a
Zhi Han <zhi.han@gmail.com>
2015-08-29 11:30:17 -0400

    Added some more tests

--- src/test/groovy/DJVMTest.groovy
+++ src/test/groovy/DJVMTest.groovy
@@ -27,8 +27,8 @@ class DJVMTest {
     DJVM.LDC(0, 0, 0.1F)
     verify(intp).LDC(0, 0, 0.1F)
 
-    DJVM.LDC(0, 0, 0.2)
-    verify(intp).LDC(0, 0, 0.2)
+    DJVM.LDC(0, 0, 0.2D)
+    verify(intp).LDC(0, 0, 0.2D)
 
     DJVM.LDC(0, 0, "a")
     verify(intp).LDC(0, 0, "a")
@@ -116,5 +116,115 @@ class DJVMTest {
 
     DJVM.IFNONNULL(0, 0, 1)
     verify(intp).IFNONNULL(0, 0, 1)
+
+    DJVM.GOTO(0, 0, 1)
+    verify(intp).GOTO(0, 0, 1)
+
+    DJVM.JSR(0, 0, 1)
+    verify(intp).JSR(0, 0, 1)
+  }
+
+  @Test
+  void testInvoke() {
+    DJVM.INVOKEVIRTUAL(0, 0, "owner", "method", "()I")
+    verify(intp).INVOKEVIRTUAL(0, 0, "owner", "method", "()I")
+
+    DJVM.INVOKESTATIC(0, 0, "owner", "method", "()I")
+    verify(intp).INVOKESTATIC(0, 0, "owner", "method", "()I")
+    
+    DJVM.INVOKESPECIAL(0, 0, "owner", "method", "()I")
+    verify(intp).INVOKESPECIAL(0, 0, "owner", "method", "()I")
+    
+    DJVM.INVOKEINTERFACE(0, 0, "owner", "method", "()I")
+    verify(intp).INVOKEINTERFACE(0, 0, "owner", "method", "()I")
+  }
+
+  @Test
+  void testField() {
+    DJVM.GETFIELD(0, 0, 1, 1, "I")
+    verify(intp).GETFIELD(0, 0, 1, 1, "I")
+
+    DJVM.PUTFIELD(0, 0, 1, 1, "I")
+    verify(intp).PUTFIELD(0, 0, 1, 1, "I")
+
+    DJVM.GETSTATIC(0, 0, 1, 1, "I")
+    verify(intp).GETSTATIC(0, 0, 1, 1, "I")
+
+    DJVM.PUTSTATIC(0, 0, 1, 1, "I")
+    verify(intp).PUTSTATIC(0, 0, 1, 1, "I")
+  }
+
+  @Test
+  void testNew() {
+    DJVM.NEW(0, 0, "MyClass", 0)
+    verify(intp).NEW(0, 0, "MyClass", 0)
+  }
+
+  @Test
+  void testTypeInsn() {
+    DJVM.ANEWARRAY(0, 0, "MyClass")
+    verify(intp).ANEWARRAY(0, 0, "MyClass")
+
+    DJVM.CHECKCAST(0, 0, "MyClass")
+    verify(intp).CHECKCAST(0, 0, "MyClass")
+
+    DJVM.INSTANCEOF(0, 0, "MyClass")
+    verify(intp).INSTANCEOF(0, 0, "MyClass")
+  }
+
+  @Test
+  void testVarInsn(){
+    DJVM.BIPUSH(0, 0, 1)
+    verify(intp).BIPUSH(0, 0, 1)
+
+    DJVM.SIPUSH(0, 0, 1)
+    verify(intp).SIPUSH(0, 0, 1)
+
+    DJVM.ILOAD(0, 0, 1)
+    verify(intp).ILOAD(0, 0, 1)
+
+    DJVM.ISTORE(0, 0, 1)
+    verify(intp).ISTORE(0, 0, 1)
+
+    DJVM.LLOAD(0, 0, 1)
+    verify(intp).LLOAD(0, 0, 1)
+
+    DJVM.LSTORE(0, 0, 1)
+    verify(intp).LSTORE(0, 0, 1)
+
+    DJVM.FLOAD(0, 0, 1)
+    verify(intp).FLOAD(0, 0, 1)
+
+    DJVM.FSTORE(0, 0, 1)
+    verify(intp).FSTORE(0, 0, 1)
+
+    DJVM.DLOAD(0, 0, 1)
+    verify(intp).DLOAD(0, 0, 1)
+
+    DJVM.DSTORE(0, 0, 1)
+    verify(intp).DSTORE(0, 0, 1)
+
+    DJVM.ALOAD(0, 0, 1)
+    verify(intp).ALOAD(0, 0, 1)
+
+    DJVM.ASTORE(0, 0, 1)
+    verify(intp).ASTORE(0, 0, 1)
+
+    DJVM.RET(0, 0, 1)
+    verify(intp).RET(0, 0, 1)
+  }
+
+
+  @Test
+  void testAllInsn() {
+    DJVM.NOP(0, 0)
+    verify(intp).NOP(0, 0)
+
+    DJVM.NEWARRAY(0, 0)
+    verify(intp).NEWARRAY(0, 0)
+
+    DJVM.ACONST_NULL(0, 0)
+    verify(intp).ACONST_NULL(0, 0)
+
   }
 }
