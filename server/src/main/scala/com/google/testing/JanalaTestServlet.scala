7a2cd92d110d7fcfd9c3318bc41e1688bd64e5a1
zhihan <zhi.han@gmail.com>
2016-01-17 15:43:00 -0500

    Remove jade template. Add bootstrap. Maybe make it a simple angular app

--- server/src/main/scala/com/google/testing/JanalaTestServlet.scala
+++ server/src/main/scala/com/google/testing/JanalaTestServlet.scala
@@ -1,15 +1,16 @@
 package com.google.testing
 
-import org.scalatra._
-import scalate.ScalateSupport
+import org.scalatra.ScalatraServlet
 
-class JanalaTestServlet extends MyTestServerStack {
+class JanalaTestServlet extends ScalatraServlet {
 
   get("/") {
     <html>
+      <meta charset="utf=8" />
+      <title>Janala Test Service</title>
+      <link rel="stylesheet" href="css/bootstrap.css" />
       <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        <h1>No test is running</h1>
       </body>
     </html>
   }
